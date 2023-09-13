c270a668cebdfddd9d1e2d4e112f1e78fc82f8a1
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-09-13 22:00:03 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I&apos;m <span className='text-[#915EFF]'>Ibrahim</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a software engineer. I design system <br className='sm:block hidden' />
-            I build applications
+            I am a software engineer. I am a database engineer <br className='sm:block hidden' />
+            Cyber Secuirty Expert
           </p>
         </div>
       </div>
