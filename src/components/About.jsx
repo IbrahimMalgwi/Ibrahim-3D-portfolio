54854d1b099780c11fbe7d8eaa6c0d608a3ca988
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-09-13 18:11:07 +0100

    3D portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/prop-types */
+// import React from "react";
+import { motion } from "framer-motion";
+import { Tilt } from "react-tilt";
+
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a software Engineer with 3+ years experience. I have knowledge of Java, JavaScript, Python, PHP, versions Control (Git), Spring Boot, React, NextJs, Django, MySQL, MongoDB, PostgreSQL, Maven, Database Administrator, and Rest APIs. I have successfully completed designing and building Solutions and implementations. I am experienced in the entire software development life cycle, from requirements gathering to deployment. I am actively seeking a new opportunity as a Software Engineer where I can leverage my skills to create high-quality, robust, and scalable applications. 
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
